d5c95fcfdf88f856f21e9da7cc5de5bdda471ac1
Finn van der Ham <533500@student.glu.nl>
2020-05-15 11:35:32 +0200

    Revert "Merge branch 'master' of https://github.com/Fiananan/Project-Groep-9"
    This reverts commit 6385bbbe3f2aa802650beb1fb2a7df344aaa17c6, reversing
    changes made to 56d4c92a64e2e8025ecdb61814e49dea1005caa0.

---
+++ Twin Stick Shooter Team 9/Assets/Scripts/Shooting.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField] private Transform ShootingPoint;
+    [SerializeField] private GameObject BulletPrefab;
+    [SerializeField] private KeyCode Fire;
+    [SerializeField] private KeyCode ReloadKey;
+
+    [SerializeField] private string WeaponEquipped;
+    [SerializeField] private GameObject Pistol;
+    [SerializeField] private Transform PistolFirePoint;
+    [SerializeField] private Weapon PistolStats;
+    [SerializeField] private GameObject AssaultRifle;
+    [SerializeField] private Transform AssaultRifleFirePoint;
+    [SerializeField] private Weapon AssaultRifleStats;
+
+    [SerializeField] private float FireRate;
+    [SerializeField] private float FireTimer;
+    [SerializeField] private int Ammo;
+    [SerializeField] private int MaxMagazineAmmo;
+    [SerializeField] private int AllAmmo;
+    [SerializeField] private float BulletSpeed;
+    [SerializeField] private bool Reloading;
+    [SerializeField] private float ReloadTime;
+    [SerializeField] private float TimeTakesToReload;
+    [SerializeField] private int WeaponDamage;
+
+    [SerializeField] private int CriticalChance;
+
+    [SerializeField] private Text AmmoText;
+
+    void Update()
+    {
+        if (Reloading)
+        {
+            ReloadTime += Time.deltaTime;
+            if (ReloadTime >= TimeTakesToReload)
+            {
+                ReloadTime = 0f;
+                Reloading = false;
+            }
+        }
+
+        EquipWeapon();
+
+        GetShootInput();
+        
+        if (Input.GetKeyDown(ReloadKey))
+        {
+            Reload();
+        }
+
+        UpdateAmmoText();
+    }
+
+    void UpdateAmmoText()
+    {
+        AmmoText.text = Ammo + "/" + AllAmmo;
+    }
+
+    void GetShootInput()
+    {
+        if (Input.GetKey(Fire))
+        {
+            if (!Reloading)
+            {
+                if (Ammo >= 1)
+                {
+                    if (FireTimer <= 0)
+                    {
+                        Shoot();
+                        FireTimer = FireRate;
+                    }
+                }
+                else
+                {
+                    Reload();
+                }
+            }
+
+            FireTimer -= Time.deltaTime;
+        }
+        else
+        {
+            FireTimer -= Time.deltaTime;
+        }
+    }
+
+    void EquipWeapon()
+    {
+        if (WeaponEquipped == "Pistol")
+        {
+            WeaponDamage = PistolStats.Damage;
+            FireRate = PistolStats.FireRate;
+            MaxMagazineAmmo = PistolStats.MagazineSize;
+            TimeTakesToReload = PistolStats.ReloadTime;
+            ShootingPoint = PistolFirePoint;
+            Pistol.SetActive(true);
+            AssaultRifle.SetActive(false);
+        }
+        else if (WeaponEquipped == "Assault Rifle")
+        {
+            WeaponDamage = AssaultRifleStats.Damage;
+            FireRate = AssaultRifleStats.FireRate;
+            MaxMagazineAmmo = AssaultRifleStats.MagazineSize;
+            TimeTakesToReload = AssaultRifleStats.ReloadTime;
+            ShootingPoint = AssaultRifleFirePoint;
+            Pistol.SetActive(false);
+            AssaultRifle.SetActive(true);
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject Bullet;
+        Bullet = Instantiate(BulletPrefab, ShootingPoint.position, ShootingPoint.rotation);
+        Bullet.GetComponent<Rigidbody>().AddForce(-Bullet.transform.forward.normalized * BulletSpeed, ForceMode.Impulse);
+        Bullet.GetComponent<Bullet>().Damage = CriticalHit();
+        Ammo--;
+    }
+
+    int CriticalHit()
+    {
+        int CritChance = Random.Range(1, CriticalChance + 1);
+
+        if (CritChance == 3)
+        {
+            return Mathf.RoundToInt(WeaponDamage + (WeaponDamage / 2));
+        }
+
+        return WeaponDamage;
+    }
+
+    void Reload()
+    {
+        if (Ammo >= MaxMagazineAmmo)
+        {
+            Ammo = MaxMagazineAmmo;
+        }
+        else if (AllAmmo >= 1)
+        {
+            Ammo = MaxMagazineAmmo;
+            AllAmmo -= MaxMagazineAmmo;
+
+            if (AllAmmo < 0)
+            {
+                AllAmmo = 0;
+            }
+            Reloading = true;
+        }
+    }
+
+    void PickupAmmo(int amount)
+    {
+        AllAmmo += amount;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ARPickup")
+        {
+            WeaponEquipped = "Assault Rifle";
+            Destroy(other.gameObject);
+        }
+    }
+}
